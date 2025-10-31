64c5ae27bb9667e38df632e0ec9da88d93dd8c9d
JanekBo1 <78037972+JanekBo1@users.noreply.github.com>
2025-10-31 16:27:58 +0100

    Procedural mine gen
    mines don't spawn on a start box

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 abcMap = {1 : "A", 2 : "B", 3 : "C", 4 : "D", 5 : "E", 6 : "F"}
 RabcMap = {"A" : 1, "B" : 2, "C" : 3, "D" : 4, "E" : 5, "F" : 6}
 import time
+import random
 
 b6x6 = [
     {"A1":"■", "B1": "■", "C1": "■", "D1": "■", "E1": "■", "F1": "■"},
@@ -13,41 +14,184 @@ b6x6 = [
 ]
 b6x6m = [
     {"A1":"□", "B1": "□", "C1": "□", "D1": "□", "E1": "□", "F1": "□"},
-    {"A2":"□", "B2": "1", "C2": "1", "D2": "1", "E2": "□", "F2": "□"},
-    {"A3":"□", "B3": "1", "C3": "X", "D3": "1", "E3": "□", "F3": "□"},
-    {"A4":"□", "B4": "1", "C4": "1", "D4": "1", "E4": "□", "F4": "□"},
-    {"A5":"□", "B5": "□", "C5": "□", "D5": "1", "E5": "1", "F5": "1"},
-    {"A6":"□", "B6": "□", "C6": "□", "D6": "1", "E6": "X", "F6": "1"},
+    {"A2":"□", "B2": "□", "C2": "□", "D2": "□", "E2": "□", "F2": "□"},
+    {"A3":"□", "B3": "□", "C3": "□", "D3": "□", "E3": "□", "F3": "□"},
+    {"A4":"□", "B4": "□", "C4": "□", "D4": "□", "E4": "□", "F4": "□"},
+    {"A5":"□", "B5": "□", "C5": "□", "D5": "□", "E5": "□", "F5": "□"},
+    {"A6":"□", "B6": "□", "C6": "□", "D6": "□", "E6": "□", "F6": "□"},
     {}
 ]
+
+
 
 
 board = b6x6 ##board size change
 boardm = b6x6m ####
 boards = 6
 
-def boardprint():
-    for i in range(len(board)):
+
+def checkUser(let, num, boardm, boards, a, board):
+    locleter = let-1 ##let-1
+    locnum = num-1
+    check(boardm, boards, locleter, locnum, a, board)
+    locleter = let
+    locnum = num-2
+    check(boardm, boards, locleter, locnum, a, board)
+    locleter = let+1
+    locnum = num-1
+    check(boardm, boards, locleter, locnum, a, board)
+    locleter = let
+    locnum = num
+    check(boardm, boards, locleter, locnum, a, board)
+    ###diagational
+    locleter = let-1 ##let-1
+    locnum = num-2  ##num-1
+    check(boardm, boards, locleter, locnum, a, board)
+    locleter = let+1 
+    locnum = num-2
+    check(boardm, boards, locleter, locnum, a, board)
+    locleter = let+1 
+    locnum = num
+    check(boardm, boards, locleter, locnum, a, board)
+    locleter = let-1 
+    locnum = num
+    check(boardm, boards, locleter, locnum, a, board)
+
+
+def boardprint(what):
+    for i in range(len(what)):
         
         print(i,end=" ")
 
-        for j in range(len(board[0])):
+        for j in range(len(what[0])):
 
             if i == 0:
                 print(abcMap[j+1],end=" ")
             else:
                 where = abcMap[j+1] + str(i)
                 #print(i-1,where)
-                print(board[i-1][where],end=" ")
+                print(what[i-1][where],end=" ")
         print("")
 
 
 
+def check(boardm, boards, locleter, locnum, mode, board):
+    if locleter > 0 and locnum > -1 and locleter < boards+1 and locnum < boards:
+
+        buit = abcMap[locleter] + str(locnum+1)
+        if mode == "m":
+            if boardm[locnum][buit] == "□":
+                boardm[locnum][buit] = "1"
+            elif boardm[locnum][buit].isdigit() == True:
+                boardm[locnum][buit] = str(int(boardm[locnum][buit])+1)
+        elif mode == "e":
+##            if boardm[locnum][buit] == "□":
+                board[locnum][buit] = boardm[locnum][buit]
+
+boardprint(board)
+
+inp = input("where ")
+
+let = RabcMap[inp[0]] #number
+num = int(inp[1])
+print(let, num)
+
+buit = abcMap[let] + str(num)
+board[num-1][buit] = "□"
+
+checkUser(let, num, boardm, boards, "e", board)
 
 
+for g in range(5):
+    ranNum = random.randint(1, boards)
+    ranLet = random.randint(1, boards)
+    buit = abcMap[ranNum] + str(ranLet)
+    if boardm[ranLet-1][buit] != "X" and board[ranLet-1][buit] != "□":
+#       maxg = maxg + 1
+        boardm[ranLet-1][buit] = "X"
+
+for i in range(len(boardm)):
+    num = i
+    for j in range(len(boardm[0])):
+        let = j+1
+        if i != 0:
+            where = abcMap[let] + str(i)
+            if boardm[i-1][where] == "X":
+                checkUser(let, num, boardm, boards, "m", board)
+
+
+
+for i in range(len(board)):
+    num = i
+    for j in range(len(board[0])):
+        let = j+1
+        if i != 0:
+            where = abcMap[let] + str(i)
+
+            if board[i-1][where] == "□":
+                board[i-1][where] = boardm[i-1][where]
+
+
+for h in range(20):
+    for i in range(len(board)):
+        num = i
+        for j in range(len(board[0])):
+            let = j+1
+            if i != 0:
+                where = abcMap[let] + str(i)
+                if board[i-1][where] == "□":
+                    if let > 1:
+                        locleter = let-1 ##let-1
+                        locnum = num-1
+                        buit = abcMap[locleter] + str(locnum+1)
+                        if board[locnum][buit] != "⚑":
+                            board[locnum][buit] = boardm[locnum][buit]
+                    if num > 1:
+                        locleter = let 
+                        locnum = num-2 ##num-1
+                        buit = abcMap[locleter] + str(locnum+1) 
+                        if board[locnum][buit] != "⚑":
+                            board[locnum][buit] = boardm[locnum][buit]
+                    if let > 1 and num > 1:
+                        locleter = let-1 ##let -1
+                        locnum = num-2 ##num-1
+                        buit = abcMap[locleter] + str(locnum+1)  
+                        if board[locnum][buit] != "⚑":
+                            board[locnum][buit] = boardm[locnum][buit]
+                    if let < boards and num > 1:
+                        locleter = let+1 ##let +1
+                        locnum = num-2 ##num-1
+                        buit = abcMap[locleter] + str(locnum+1)  
+                        if board[locnum][buit] != "⚑":
+                            board[locnum][buit] = boardm[locnum][buit]
+                    if let < boards:
+                        locleter = let+1 ##let +1
+                        locnum = num-1 ##num
+                        buit = abcMap[locleter] + str(locnum+1)
+                        if board[locnum][buit] != "⚑":
+                            board[locnum][buit] = boardm[locnum][buit]
+                    if num < boards:
+                        locleter = let 
+                        locnum = num ##num +1
+                        buit = abcMap[locleter] + str(locnum+1)
+                        if board[locnum][buit] != "⚑":
+                            board[locnum][buit] = boardm[locnum][buit]
+                    if let > 1 and num < boards:
+                        locleter = let-1 ##let -1
+                        locnum = num ##num+1
+                        buit = abcMap[locleter] + str(locnum+1)  
+                        if board[locnum][buit] != "⚑":
+                            #print(buit)
+                            board[locnum][buit] = boardm[locnum][buit]
+                    if let < boards and num < boards:
+                        locleter = let+1 ##let +1
+                        locnum = num ##num+1
+                        buit = abcMap[locleter] + str(locnum+1)  
+                        if board[locnum][buit] != "⚑":
+                            board[locnum][buit] = boardm[locnum][buit]
 
 while True:
-    boardprint()
+    boardprint(board)
     inp = input("where")
     
     let = RabcMap[inp[0]] #number
@@ -129,7 +273,7 @@ while True:
                                 buit = abcMap[locleter] + str(locnum+1)  
                                 
                                 if board[locnum][buit] != "⚑":
-                                    print(buit)
+                                    #print(buit)
                                     board[locnum][buit] = boardm[locnum][buit]
                             if let < boards and num < boards:
                                 locleter = let+1 ##let +1
@@ -145,4 +289,4 @@ while True:
             
 
 
-boardprint()
+boardprint(boardm)
